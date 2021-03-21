63507de6e8ffdc656153836d92260110dad135e6
Samvel Ayrapetov <movetonjkee@gmail.com>
2021-03-21 13:21:11 +0300

    slider-img

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,20 @@
-// // Import jQuery module (npm i jquery)
-// import $ from 'jquery'
-// window.jQuery = $
-// window.$ = $
-
-// // Import vendor jQuery plugin example (not module)
-// require('~/app/libs/mmenu/dist/mmenu.js')
-
+import { Swiper, Parallax, Mousewheel, Controller } from 'swiper';
+Swiper.use([Parallax, Mousewheel, Controller]);
 document.addEventListener('DOMContentLoaded', () => {
+  const swiperIMG = new Swiper('.slider-img', {
+    loop: false,
+    speed: 2400,
+    parallax: true,
+  });
 
-	// Custom JS
+  const swiperText = new Swiper('.slider-text', {
+    loop: false,
+    speed: 2400,
+    mousewheel: {
+      invert: false,
+    },
+  });
 
-})
+  swiperIMG.controller.control = swiperText;
+  swiperText.controller.control = swiperIMG;
+});
